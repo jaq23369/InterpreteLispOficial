d93ec70d783f67819f9384a70cee1ea4e323d8f2
rocha30 <144186626+rocha30@users.noreply.github.com>
2024-03-13 21:52:42 -0600

    AppTest. JUnit 5.8.2

--- src/test/java/uvg/edu/gt/AppTest.java
+++ src/test/java/uvg/edu/gt/AppTest.java
@@ -1,20 +1,31 @@
-package uvg.edu.gt;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSimpleAddition() {
+        LispInterpreter interpreter = new LispInterpreter();
+        Object result = interpreter.interpret("(+ 2 3)");
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void testVariableDefinition() {
+        LispInterpreter interpreter = new LispInterpreter();
+        interpreter.defineVariable("x", 10);
+        Object result = interpreter.getVariableValue("x");
+        Assertion.assertEquals(10, result);
+    }
+
+    @Test
+    public void testFunctionDefinitionAndEvaluation() {
+        LispInterpreter interpreter = new LispInterpreter();
+        interpreter.defineFunction("cuadrado", new FunctionDefinition(Arrays.asList("x"), Arrays.asList("(* x x)")));
+        Object result = interpreter.interpret("(cuadrado 5)");
+        Assertion.assertEquals(25, result);
     }
 }
